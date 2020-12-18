4423af79ca674ad6c49e8c395051e29c2bb02be4
I Nyoman Triarta <54964375+triartainyoman@users.noreply.github.com>
2020-12-18 11:55:01 +0800

    Add files via upload
    Initial init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Vegetable Soup</li>
+      <li>Fried Rice</li>
+      <li>Pizza</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
